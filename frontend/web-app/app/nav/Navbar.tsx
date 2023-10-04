44dc320a17e73f6859b82ced273d43626e0856dd
mxbooll <mxbooll@gmail.com>
2023-10-04 08:47:58 +0500

    Раздел 8: 97

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {AiOutlineCar} from "react-icons/ai";
+import Search from "@/app/nav/Search";
 
-export default function Navbar(props) {
+export default function Navbar() {
     return (
         <header className="sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-sm">
             <div className="flex items-center gap-2 text-3xl font-semibold text-red-500">
@@ -10,7 +11,7 @@ export default function Navbar(props) {
                     Carsties Auctions
                 </div>
             </div>
-            <div>Search</div>
+            <Search />
             <div>Login</div>
         </header>
     );
