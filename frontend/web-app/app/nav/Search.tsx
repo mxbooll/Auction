4d870aeea1a6a95159b22d8b1ce5370bedb06b23
mxbooll <mxbooll@gmail.com>
2023-10-05 10:16:53 +0500

    Раздел 8: 98-99

--- frontend/web-app/app/nav/Search.tsx
+++ frontend/web-app/app/nav/Search.tsx
@@ -1,10 +1,30 @@
+'use client'
+
 import React from 'react';
 import {FaSearch} from "react-icons/fa";
+import {useParamsStore} from "@/app/hooks/useParamsStore";
 
 export default function Search() {
+    const setParams = useParamsStore(state => state.setParams);
+    const setSearchValue = useParamsStore(state => state.setSearchValue);
+    const searchValue = useParamsStore(state => state.searchValue);
+
+    function onChange(event: any) {
+        setSearchValue(event.target.value);
+    }
+
+    function search() {
+        setParams({searchTerm: searchValue});
+    }
+
     return (
         <div className='flex w-[50%] items-center border-2 rounded-full py-2 shadow-sm'>
             <input
+                onKeyDown={(e: any) => {
+                    if (e.key === 'Enter') search();
+                }}
+                onChange={onChange}
+                value={searchValue}
                 type="text"
                 placeholder='Search for cars by make, model or color'
                 className='
@@ -19,7 +39,7 @@ export default function Search() {
                     text-gray-600
                 '
             />
-            <button>
+            <button onClick={search}>
                 <FaSearch
                     size={35}
                     className='
