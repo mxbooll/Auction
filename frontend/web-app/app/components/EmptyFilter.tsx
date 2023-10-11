efe59ca53ec26543ed9fb1ce6f6cc1eab2347199
mxbooll <mxbooll@gmail.com>
2023-10-11 09:15:35 +0500

    Раздел 8: 111

--- frontend/web-app/app/components/EmptyFilter.tsx
+++ frontend/web-app/app/components/EmptyFilter.tsx
@@ -1,27 +1,37 @@
+'use client'
+
 import React from 'react';
 import {useParamsStore} from "@/app/hooks/useParamsStore";
 import Heading from "@/app/components/Heading";
 import {Button} from "flowbite-react";
+import {signIn} from "next-auth/react";
 
 type Props = {
     title?: string
     subtitle?: string
     showReset?: boolean
+    showLogin?: boolean
+    callbackUrl?: string
 }
 
 export default function EmptyFilter({
                                         title = "No matches for this filter",
                                         subtitle = "Try changing or resetting the filter",
-                                        showReset
+                                        showReset,
+                                        showLogin,
+                                        callbackUrl
                                     }: Props) {
     const reset = useParamsStore(state => state.reset);
 
     return (
         <div className='h-[40vh] flex flex-col gap-2 justify-center items-center shadow-lg'>
-            <Heading title={title!} subtitle={subtitle!} center />
+            <Heading title={title!} subtitle={subtitle!} center/>
             <div className="mt-4">
                 {showReset && (
                     <Button outline onClick={reset}>Remove Filters</Button>
+                )}
+                {showLogin && (
+                    <Button outline onClick={() => signIn('id-server', {callbackUrl})}>Login</Button>
                 )}
             </div>
         </div>
