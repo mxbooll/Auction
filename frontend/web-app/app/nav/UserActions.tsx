f14798dc5410f4924a344fce8208c7dc0440561c
mxbooll <mxbooll@gmail.com>
2023-10-16 08:09:12 +0500

    Раздел 8: 117

--- frontend/web-app/app/nav/UserActions.tsx
+++ frontend/web-app/app/nav/UserActions.tsx
@@ -7,26 +7,38 @@ import {HiUser, HiCog} from "react-icons/hi2";
 import Link from "next/link";
 import {AiFillCar, AiFillTrophy, AiOutlineLogout} from "react-icons/ai";
 import {signOut} from "next-auth/react";
+import {usePathname, useRouter} from "next/navigation";
+import {useParamsStore} from "@/app/hooks/useParamsStore";
 
 type Props = {
     user: Partial<User>
 }
 
 export default function UserActions({user}: Props) {
+    const router = useRouter();
+    const pathname = usePathname();
+    const setParams = useParamsStore(state => state.setParams);
+
+    function setWinner() {
+        setParams({winner: user.username, seller: undefined})
+        if (pathname !== '/') router.push('/');
+    }
+
+    function setSeller() {
+        setParams({seller: user.username, winner: undefined})
+        if (pathname !== '/') router.push('/');
+    }
+
     return (
         <Dropdown
             dismissOnClick={false}
             label={`Welcome ${user.name}`}
         >
-            <Dropdown.Item icon={HiUser}>
-                <Link href='/'>
-                    My Auctions
-                </Link>
+            <Dropdown.Item icon={HiUser} onClick={setSeller}>
+                My Auctions
             </Dropdown.Item>
-            <Dropdown.Item icon={AiFillTrophy}>
-                <Link href='/'>
-                    Auctions won
-                </Link>
+            <Dropdown.Item icon={AiFillTrophy} onClick={setWinner}>
+                Auctions won
             </Dropdown.Item>
             <Dropdown.Item icon={AiFillCar}>
                 <Link href='/auctions/create'>
@@ -38,7 +50,7 @@ export default function UserActions({user}: Props) {
                     Session (dev only)
                 </Link>
             </Dropdown.Item>
-            <Dropdown.Divider />
+            <Dropdown.Divider/>
             <Dropdown.Item icon={AiOutlineLogout} onClick={() => signOut({callbackUrl: "/"})}>
                 Sign out
             </Dropdown.Item>
