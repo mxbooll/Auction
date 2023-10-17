2f1e007345de31700649387d8a6cc1681a6b7dbe
mxbooll <mxbooll@gmail.com>
2023-10-17 21:06:15 +0500

    Раздел 8: 118-119

--- frontend/web-app/app/auctions/create/page.tsx
+++ frontend/web-app/app/auctions/create/page.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Heading from "@/app/components/Heading";
+import AuctionForm from "@/app/auctions/AuctionForm";
 
 export default function Create() {
     return (
-        <div>Create auction</div>
+        <div className='mx-auto max-w-[75%] shadow-lg p-10 bg-white rounded-lg'>
+            <Heading title='Sell your car!' subtitle='Please enter details of your car' />
+            <AuctionForm />
+        </div>
     );
 }
